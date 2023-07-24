8e052773bd068317542c1e4798230c20b3201ac8
igrynok <ibragimova.aliya@gmail.com>
2023-07-24 23:07:33 +0200

    Add unit tests to test play method with a mock in Player.

--- tests/game/test_player.py
+++ tests/game/test_player.py
@@ -1,5 +1,6 @@
 import unittest
 from game.player import Player
+from unittest.mock import patch
 
 
 class TestPlayer(unittest.TestCase):
@@ -8,3 +9,14 @@ class TestPlayer(unittest.TestCase):
         player = Player('Mike')
         self.assertEqual(player.name, 'Mike')
         self.assertEqual(player.score, 0)
+
+    def test_play(self):
+        player = Player('John')
+
+        def mock_play(indices, deck):
+            return indices[:2]
+
+        with patch.object(Player, 'play', side_effect=mock_play):
+            result = player.play([0, 1, 2, 3], 'X X X X')
+
+        self.assertEqual(result, [0, 1])
